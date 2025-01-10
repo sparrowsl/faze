3ad45ba7054b4a7b0cea208933a41b2b0e0b651d
sparrowsl <benthorpe.dev@gmail.com>
2025-01-10 11:32:35 +0000

    feat: reply only to specified channels

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import { Client, Events, GatewayIntentBits } from "discord.js";
 
+const reply_to_channels = ["payments"];
+
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
@@ -16,18 +18,24 @@ client.once(Events.ClientReady, (c) => {
 client.on(Events.MessageCreate, async (message) => {
 	// don't reply if the message was sent by the bot.
 	if (message.author.bot === true) {
+		// FIX: delete the line below
 		setTimeout(() => message.delete(), 5000);
 		return;
 	}
 
-	await message.reply(`New Payment from Bot...
+	// TODO: decide to reply only to specific bot username.
+	// Only reply to the payments channel if found.
+	if (reply_to_channels.includes(message.channel.name)) {
+		await message.reply(`New Payment Receipt from Bot (faze)...
 Name: Jack Daniels
 Contact: +23299783218
 Receipt Number: Check payment receipt
 Amount Remitted: $50
 Sender Receipt: https://www.url_link.com`);
 
-	setTimeout(() => message.delete(), 2000);
+		// FIX: delete the line below
+		setTimeout(() => message.delete(), 2000);
+	}
 });
 
 client.login(process.env.BOT_TOKEN);
